ebdf3f69a0ae41253edcfc45659664eb0ff43fee
Olly Pethick <ollypethick@gmail.com>
2023-08-03 14:21:32 +0100

    style: add styling to holding page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,45 @@
-import { Button, Header } from "ui";
 import logo from "./img/logo.png";
+import { Typography, Box } from "@mui/material";
 
 export default function Page() {
   return (
-    <>
-      <img src={logo.src} alt="Gobblet Logo" />
-      <p>Coming Soon</p>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "column",
+        alignItems: "center",
+        background: "linear-gradient(45deg, #3244ca 30%, #171ea4 90%)",
+        height: "100svh",
+        width: "100svw",
+      }}
+    >
+      <Box
+        sx={{
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          maxWidth: "500px",
+          margin: "auto",
+          padding: "1rem",
+
+          img: {
+            width: "100%",
+          },
+        }}
+      >
+        <img src={logo.src} alt="Gobblet Logo" />
+        <Typography
+          sx={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: ["1rem", "2rem"],
+            marginTop: "1rem",
+          }}
+          variant="body1"
+        >
+          Coming Soon
+        </Typography>
+      </Box>
+    </Box>
   );
 }
