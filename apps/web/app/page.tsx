b7f0f637853971d441df8d27cc957aa78c3d4cf0
Olly Pethick <ollypethick@gmail.com>
2023-08-03 13:51:04 +0100

    feat: add logo and coming soon text

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button, Header } from "ui";
+import logo from "./img/logo.png";
 
 export default function Page() {
   return (
     <>
-      <Header text="Web" />
-      <Button />
+      <img src={logo.src} alt="Gobblet Logo" />
+      <p>Coming Soon</p>
     </>
   );
 }
